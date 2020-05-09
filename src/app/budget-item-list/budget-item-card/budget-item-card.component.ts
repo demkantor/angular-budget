0a03d6c8a65c2e182bc578bf85197c6111e54b8c
demkantor <demkantor@gmail.com>
2020-05-09 01:07:41 -0500

    logic for adding new income and expenses in place

--- src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
+++ src/app/budget-item-list/budget-item-card/budget-item-card.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { BudgetItem } from '../../../shared/models/budget-item.model';
 
 @Component({
   selector: 'app-budget-item-card',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-item-card.component.scss']
 })
 export class BudgetItemCardComponent implements OnInit {
+
+  @Input() item: BudgetItem;
 
   constructor() { }
 
