4d6b183bf7f0794d831efbca5fe618bf2e12b512
demkantor <demkantor@gmail.com>
2020-05-09 09:45:13 -0500

    fixed some modal issues, brought in bool on add/save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
+import { MatDialogModule } from '@angular/material/dialog';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainPageComponent } from './main-page/main-page.component';
@@ -9,6 +9,7 @@ import { AddItemFormComponent } from './add-item-form/add-item-form.component';
 import { BudgetItemListComponent } from './budget-item-list/budget-item-list.component';
 import { BudgetItemCardComponent } from './budget-item-list/budget-item-card/budget-item-card.component';
 import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -22,9 +23,12 @@ import { EditItemModalComponent } from './edit-item-modal/edit-item-modal.compon
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule
   ],
   providers: [],
+  entryComponents: [EditItemModalComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/edit-item-modal/edit-item-modal.component.ts
+++ src/app/edit-item-modal/edit-item-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { BudgetItem } from 'src/shared/models/budget-item.model';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-edit-item-modal',
@@ -8,15 +9,15 @@ import { BudgetItem } from 'src/shared/models/budget-item.model';
 })
 export class EditItemModalComponent implements OnInit {
 
-  @Input() item: BudgetItem;
-
-  constructor() { }
+  constructor(
+    public dialogRef: MatDialogRef<EditItemModalComponent>,
+    @Inject(MAT_DIALOG_DATA) public item: BudgetItem) { }
 
   ngOnInit(): void {
   }
 
   onSubmitted(updatedItem: BudgetItem) {
-
+    this.dialogRef.close(updatedItem);
   }
 
 }
